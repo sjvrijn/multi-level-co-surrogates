3383a91e7086edd241bcaf8adf2a829fd8f075fd
Sander van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2017-10-02 17:44:34 +0200

    small cleanup of config

--- multiLevelCoSurrogates/config.py
+++ multiLevelCoSurrogates/config.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 '''
-config.py: Simple python file to store a bunch of variable settings that are re-used in multiple files
+config.py: Simple python file to store variable settings that are re-used in multiple files
 '''
 
 __author__ = 'Sander van Rijn'
@@ -11,14 +11,13 @@ __email__ = 's.j.van.rijn@liacs.leidenuniv.nl'
 
 
 import cma
-import numpy as np
 
 
 # Filename related settings
 plot_dir = 'plots/'
 data_dir = 'data/'
 filename = '{dim}D-{func}'
-suffix   = '-s{size}-r{rep}-'  # '-s{size}-r{rep}-'
+suffix   = '-s{size}-r{rep}-'
 plot_ext = '.png'  # Choose from: ['.png', '.pdf']
 data_ext = '.dat'
 
@@ -33,6 +32,5 @@ fit_funcs = {
 }
 
 # Experimental parameters
+training_size = 50
 experiment_repetitions = 30
-experiment_intervals   = [1, 2, 5, 10, 15, 20, 25, 50, 75, 100, 150, 200]
-experiment_thresholds  = np.arange(0.5, 8, 0.5)
