812200c5a2f8b4439680c2e35d773a938edbe504
Sander van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2017-10-09 14:57:06 +0200

    experiment setup adapted to use the given upper and lower bounds

--- multiLevelCoSurrogates/__main__.py
+++ multiLevelCoSurrogates/__main__.py
@@ -17,8 +17,33 @@ from functools import partial
 
 from multiLevelCoSurrogates.Surrogates import RBF, Kriging
 from multiLevelCoSurrogates.Logger import Logger
-from multiLevelCoSurrogates.config import data_dir, filename, suffix, data_ext, fit_funcs
+from multiLevelCoSurrogates.config import data_dir, filename, suffix, data_ext, fit_funcs, fit_func_dims
 from multiLevelCoSurrogates.config import experiment_repetitions, training_size
+
+
+def _keepInBounds(x, l_bound, u_bound):
+    """
+        This function transforms x to t w.r.t. the low and high
+        boundaries lb and ub. It implements the function T^{r}_{[a,b]} as
+        described in Rui Li's PhD thesis "Mixed-Integer Evolution Strategies
+        for Parameter Optimization and Their Applications to Medical Image
+        Analysis" as alorithm 6.
+
+        :param x:       Column vector to be kept in bounds
+        :param l_bound: Lower bound column vector
+        :param u_bound: Upper bound column vector
+        :returns:       An in-bounds kept version of the column vector ``x``
+    """
+
+    y = (x - l_bound) / (u_bound - l_bound)
+    floor_y = np.floor(y)                              # Local storage to prevent double calls
+    I = np.mod(floor_y, 2) == 0
+    yprime = np.zeros(np.shape(y))
+    yprime[I] = np.abs(y[I] - floor_y[I])
+    yprime[~I] = 1.0 - np.abs(y[~I] - floor_y[~I])
+
+    x = l_bound + (u_bound - l_bound) * yprime
+    return x
 
 
 def createSurrogate(N, init_sample_size, fit_func, Surrogate):
@@ -63,10 +88,10 @@ def runMultiFidelityExperiment(N, lambda_, lambda_pre, mu, init_sample_size,
                                fit_func_name, rep):
 
     ### SETUP ###
-    Surrogate = Kriging
+    Surrogate = RBF
     fit_func = fit_funcs[fit_func_name]
     sigma = 0.5
-    init_individual = N * [0]
+    init_individual = [(u+l)/2 for u, l in zip(fit_func.u_bound, fit_func.l_bound)]
     archive_candidates = []
     gen_counter = 0
 
@@ -94,18 +119,22 @@ def runMultiFidelityExperiment(N, lambda_, lambda_pre, mu, init_sample_size,
     else:
         std_log = None
 
+    l_bound = np.array(fit_func.l_bound)
+    u_bound = np.array(fit_func.u_bound)
+
 
     ### OPTIMIZATION ###
     while not es.stop():
         # Obtain the list of lambda_pre candidates to evaluate
-        candidates = np.array(es.ask())
+        candidates = es.ask()
+        candidates = np.array([_keepInBounds(cand, l_bound, u_bound) for cand in candidates])
         low_results = [fit_func.low(cand) for cand in candidates]
         low_errors = surrogate.predict(candidates)
         pre_results = [a + b for a, b in zip(low_results, low_errors)]
 
         results = preSelection(candidates, pre_results, lambda_, fit_func, archive_candidates)
         es.tell(candidates, results)
-        full_res_log.writeLine([fit_func(cand) for cand in candidates])
+        full_res_log.writeLine([fit_func.high(cand) for cand in candidates])
 
         # Write data to disc to be plotted
         if std_log:
@@ -122,24 +151,23 @@ def runMultiFidelityExperiment(N, lambda_, lambda_pre, mu, init_sample_size,
 
 
 def run():
-
-    N = 8
-    lambda_ = 4+int(3*np.log(N))
-    lambda_pre = 2*lambda_
-    mu = lambda_//2
     init_sample_size = 20
 
     num_reps = experiment_repetitions
-
     fit_func_names = fit_funcs.keys()
+    experiments = product(fit_func_names, range(num_reps))
 
-    experiments = product(range(num_reps), fit_func_names)
-    for rep, fit_func_name in experiments:
+    for fit_func_name, rep in experiments:
 
-        print("\n\n"
-              "---------------------------------------------\n"
-              "Function:           {fname}\n"
-              "Repetittion:        {rep}".format(fname=fit_func_name, rep=rep))
+        N = fit_func_dims[fit_func_name]
+        lambda_ = 4 + int(3 * np.log(N))
+        lambda_pre = 2 * lambda_
+        mu = lambda_ // 2
+
+        print(f"\n\n"
+              f"---------------------------------------------\n"
+              f"Function:           {fit_func_name}\n"
+              f"Repetittion:        {rep}")
 
         runMultiFidelityExperiment(N, lambda_, lambda_pre, mu, init_sample_size, fit_func_name, rep)
 
