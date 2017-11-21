7706bbeee4f43fcb1bcda1994294781a88acc377
Sander van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2017-11-21 16:43:36 +0100

    Setup for CMA-ES in EGO updated

--- multiLevelCoSurrogates/BayesianOptimization.py
+++ multiLevelCoSurrogates/BayesianOptimization.py
@@ -15,25 +15,35 @@ from scipy.special import erf
 
 
 class EGO:
-    def __init__(self, surrogate, ndims):
+    def __init__(self, surrogate, ndims, u_bound, l_bound):
         self.surrogate = surrogate
         self.ndims = ndims
         self.y_best = min(surrogate.y)
+        self.x0 = [(u+l)/2 for u, l in zip(u_bound, l_bound)]
+        self.sigma0 = 0.9*min([u-l for u, l in zip(u_bound, l_bound)])
+        self.cma_opts = {'bounds': [l_bound, u_bound],
+                         'maxiter': 250,
+                         'verbose': -9,
+                         'verb_disp': 0,
+                         'verb_log': 0,
+                         'verb_time': False}
+
 
     def next_infill(self, verbose=False):
 
         if verbose:
             print('CMA-ES run for optimizing EI...')
-        x0 = [0.5] * self.ndims
-        sigma0 = 1
-        es = cma.CMAEvolutionStrategy(x0, sigma0).optimize(self.exp_imp)
+
+        es = cma.CMAEvolutionStrategy(self.x0, self.sigma0, inopts=self.cma_opts).optimize(self.exp_imp)
         x, EI = es.result.xbest, es.result.fbest
         if verbose:
             print('CMA-ES run complete.')
         return x, EI
 
     def exp_imp(self, x):
-        y_hat, SSqr = self.surrogate.predict(x)
+        x = x.reshape(1,-1)
+        y_hat = self.surrogate.predict(x)
+        SSqr = self.surrogate.predict_std(x)
 
         if SSqr == 0:
             EI = 0
