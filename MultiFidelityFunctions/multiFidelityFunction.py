c10719c7e0e1eeeeef1407400e3a073b85e6e14e
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2018-10-04 13:25:24 +0200

    Removed internal folder for multi-fidelity functions, this is now it's own repository and (offline) package

--- MultiFidelityFunctions/multiFidelityFunction.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from collections import namedtuple
-
-BiFidelityFunction = namedtuple('BiFidelityFunction', ['u_bound', 'l_bound', 'high', 'low'])
-TriFidelityFunction = namedtuple('TriFidelityFunction', ['u_bound', 'l_bound', 'high', 'medium', 'low'])
