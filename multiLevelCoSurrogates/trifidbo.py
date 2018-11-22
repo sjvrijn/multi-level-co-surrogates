4db740eec2a4b4cf559a4a8bda5c68b04f22c346
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2018-11-22 17:10:59 +0100

    bounds passed properly to acq_function; acquisition function plotted now it finally works; function explicitly inverted to maximization problem to work with acquisition function

--- multiLevelCoSurrogates/trifidbo.py
+++ multiLevelCoSurrogates/trifidbo.py
@@ -8,6 +8,7 @@ from functools import partial
 from multiLevelCoSurrogates.bo import gpplot, createsurfaces, plotsurfaces, ScatterPoints
 
 from pyDOE import lhs
+from multifidelityfunctions import TriFidelityFunction
 from multiLevelCoSurrogates.config import fit_funcs
 from multiLevelCoSurrogates.local import base_dir
 from multiLevelCoSurrogates.Utils import select_subsample, linearscaletransform, ValueRange
@@ -24,7 +25,7 @@ def run(multi_fid_func, num_iters=100):
     fidelities = list(multi_fid_func.fidelity_names)
     bounds = np.array([multi_fid_func.l_bound, multi_fid_func.u_bound], dtype=np.float)
 
-    schema = [9,3,1]
+    schema = [4,2,1]
 
     samples = create_sample_set(ndim, zip(fidelities, [3,5,10]),
                                 desired_range=ValueRange(*bounds))
@@ -46,12 +47,10 @@ def run(multi_fid_func, num_iters=100):
 
     high_model.train()
 
-    util = bo.helpers.UtilityFunction(kind='ei', kappa=2.576, xi=0.5)
-    def acq_func(*args, **kwargs):
-        return -util.utility(*args, **kwargs)
+    utility = bo.helpers.UtilityFunction(kind='ei', kappa=2.576, xi=0.0).utility
     acq_max = partial(bo.helpers.acq_max,
-                      ac=acq_func, gp=high_model, random_state=np.random.RandomState(),
-                      bounds=bounds, n_warmup=1000, n_iter=50)
+                      ac=utility, gp=high_model, random_state=np.random.RandomState(),
+                      bounds=bounds.T, n_warmup=1000, n_iter=50)
 
     functions_to_plot = [
         multi_fid_func.high, multi_fid_func.medium, multi_fid_func.low,
@@ -59,34 +58,39 @@ def run(multi_fid_func, num_iters=100):
         partial(gpplot, func=high_model.predict),
         partial(gpplot, func=mid_model.predict),
         partial(gpplot, func=low_model.predict),
+
+        lambda x: utility(x, gp=high_model, y_max=archive.max['high']),
+        lambda x: utility(x, gp=mid_model, y_max=archive.max['medium']),
+        lambda x: utility(x, gp=low_model, y_max=archive.max['low']),
     ]
     titles = [
         'high', 'medium', 'low',
         'high model', 'medium model', 'low model',
+        'acq_high', 'acq_medium', 'acq_low',
     ]
 
     for iteration in range(num_iters):
         next_point = acq_max(y_max=archive.max['high'])
-        print('next:', next_point)
 
         for interval, fidelity in zip(schema, fidelities):
-            print('\t', iteration, interval, iteration%interval)
             if iteration % interval == 0:
                 next_value = getattr(multi_fid_func, fidelity)(next_point)
                 archive.addcandidate(next_point, next_value, fidelity=fidelity)
-        print(len(archive))
+        print('iteration:', iteration, 'archive_size:', len(archive))
         high_model.train()
 
         red_dot = {'marker': '.', 'color': 'red'}
-        surfaces = createsurfaces(functions_to_plot)
+        blue_circle = {'marker': 'o', 'facecolors': 'none', 'color': 'blue'}
+        green_cross = {'marker': '+', 'color': 'green'}
+        surfaces = createsurfaces(functions_to_plot, l_bound=multi_fid_func.l_bound, u_bound=multi_fid_func.u_bound)
         points = [
             [ScatterPoints(*archive.getcandidates(fidelity='high'), style=red_dot)],
-            [ScatterPoints(*archive.getcandidates(fidelity='medium'), style=red_dot)],
-            [ScatterPoints(*archive.getcandidates(fidelity='low'), style=red_dot)],
-        ]*2
+            [ScatterPoints(*archive.getcandidates(fidelity='medium'), style=blue_circle)],
+            [ScatterPoints(*archive.getcandidates(fidelity='low'), style=green_cross)],
+        ]*3
 
         if (iteration%9) == 0:
-            plotsurfaces(surfaces, all_points=points, titles=titles, as_3d=True, shape=(2,3))
+            plotsurfaces(surfaces, all_points=points, titles=titles, as_3d=False, shape=(3,3))
 
 
 
@@ -108,4 +112,11 @@ def create_sample_set(ndim, size_per_fidelity, desired_range=None):
 
 if __name__ == '__main__':
     np.set_printoptions(linewidth=200)
-    run(fit_funcs['himmelblau'])
+
+    old_hm = fit_funcs['himmelblau']
+    hm = TriFidelityFunction(
+        u_bound=old_hm.u_bound, l_bound=old_hm.l_bound,
+        high=lambda x: -old_hm.high(x), medium=lambda x: -old_hm.medium(x), low=lambda x: -old_hm.low(x)
+    )
+
+    run(hm)
