758e8f2fc3086452e372ae6b27fe0be277032c0c
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2019-01-17 14:02:48 +0100

    Added some imports directly in __init__ to make life a lot easier

--- multiLevelCoSurrogates/__init__.py
+++ multiLevelCoSurrogates/__init__.py
@@ -8,3 +8,8 @@ Filename.py: << A short summary docstring about this file >>
 __author__ = 'Sander van Rijn'
 __email__ = 's.j.van.rijn@liacs.leidenuniv.nl'
 
+
+from .CandidateArchive import CandidateArchive
+from .multiFidBO import MultiFidelityBO
+from .Surrogates import Surrogate, CoSurrogate, HierarchicalSurrogate
+from .Utils import create_random_sample_set, sample_by_function
