b072fc0aac295ffa3c75b7b254d24b58dc72be55
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2019-02-15 16:20:24 +0100

    refactoring imports, prep for making 'multiLevelCoSurrogates' (more) usable as a namespace

--- multiLevelCoSurrogates/__init__.py
+++ multiLevelCoSurrogates/__init__.py
@@ -9,7 +9,10 @@ __author__ = 'Sander van Rijn'
 __email__ = 's.j.van.rijn@liacs.leidenuniv.nl'
 
 
-from .CandidateArchive import CandidateArchive
-from .multiFidBO import MultiFidelityBO
-from .Surrogates import Surrogate, CoSurrogate, HierarchicalSurrogate
-from .Utils import create_random_sample_set, sample_by_function
+from .BayesianOptimization import *
+from .CandidateArchive import *
+from .Surrogates import *
+from .Utils import *
+
+from .bo import *
+from .multiFidBO import *
