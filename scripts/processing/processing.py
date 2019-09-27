c82f7001f37282e0026becc8438a93a434cdbd8d
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2019-09-27 11:40:19 +0200

    Added extents to imshow plotting in processing.py; i.e. axis now labeled properly instead of starting at (0,0)

--- scripts/processing/processing.py
+++ scripts/processing/processing.py
@@ -26,6 +26,15 @@ single_point_styles = [{'marker': m}  # 'fillstyle': f,
                        #for f, m in product(['full', None], 'oSHDPX*v^><')]
 
 
+def get_extent(data):
+    return [
+        np.min(data.n_low) - 0.5,
+        np.max(data.n_low) - 0.5,
+        np.min(data.n_high) - 0.5,
+        np.max(data.n_high) - 0.5,
+    ]
+
+
 def plot_high_vs_low_num_samples(data, title, vmin=.5, vmax=100,
                                  points=(), save_as=None):
     norm = colors.LogNorm(vmin=vmin, vmax=vmax, clip=True)
@@ -35,7 +44,8 @@ def plot_high_vs_low_num_samples(data, title, vmin=.5, vmax=100,
     data = data.median(dim='rep')
 
     plt.title(f'Median MSE for high (hierarchical) model - {title}')
-    img = ax.imshow(data.sel(model='high_hier'), cmap='viridis_r', norm=norm, origin='lower')
+    img = ax.imshow(data.sel(model='high_hier'), cmap='viridis_r', norm=norm,
+                    origin='lower', extent=get_extent(data))
 
     divider = make_axes_locatable(ax)
     axx = divider.append_axes("bottom", size=.2, pad=0.05, sharex=ax)
@@ -46,8 +56,12 @@ def plot_high_vs_low_num_samples(data, title, vmin=.5, vmax=100,
     axy.xaxis.set_tick_params(labelbottom=False)
     axx.yaxis.set_tick_params(labelleft=False)
 
-    img = axy.imshow(data.sel(model='high').mean(dim='n_low').values.reshape(-1,1), cmap='viridis_r', norm=norm, origin='lower')
-    img = axx.imshow(data.sel(model='low').mean(dim='n_high').values.reshape(1,-1), cmap='viridis_r', norm=norm, origin='lower')
+    img = axy.imshow(data.sel(model='high').mean(dim='n_low').values.reshape(-1,1),
+                     cmap='viridis_r', norm=norm, origin='lower',
+                     extent=(-0.5, 0.5, np.min(data.n_high)-.5, np.max(data.n_high)-.5))
+    img = axx.imshow(data.sel(model='low').mean(dim='n_high').values.reshape(1,-1),
+                     cmap='viridis_r', norm=norm, origin='lower',
+                     extent=(np.min(data.n_low)-.5, np.max(data.n_low)-.5, -0.5, 0.5))
 
     pts = []
     for p, style in zip(points, single_point_styles):
@@ -80,11 +94,14 @@ def plot_high_vs_low_num_samples_diff(data, title, max_diff=None, save_as=None):
 
     long_title = f'Median of paired (high (hierarchical) - high (direct)) MSE - {title}'
 
-    plot_high_v_low(long_title, norm, save_as, to_plot)
+    plot_high_v_low_diff(to_plot, long_title, norm, save_as)
 
 
-def plot_inter_method_diff(data_A, data_B, name, max_diff=None, save_as=None):
-    to_plot = np.nanmedian(data_A.sel(model='high_hier') - data_B.sel(model='high_hier'), axis=2)
+def plot_inter_method_diff(data_A, data_B, name, model='high_hier',
+                           max_diff=None, save_as=None):
+
+    paired_diffs = data_A.sel(model=model) - data_B.sel(model=model)
+    to_plot = paired_diffs.median(dim='rep')
 
     if max_diff is None:
         max_diff = 2*min(abs(np.nanmin(to_plot)), np.nanmax(to_plot))
@@ -93,12 +110,14 @@ def plot_inter_method_diff(data_A, data_B, name, max_diff=None, save_as=None):
 
     long_title = f'high (hierarchical) MSE: {name}'
 
-    plot_high_v_low(long_title, norm, save_as, to_plot)
+    plot_high_v_low_diff(to_plot, long_title, norm, save_as)
 
 
-def plot_high_v_low(long_title, norm, save_as, to_plot):
+def plot_high_v_low_diff(to_plot, long_title, norm, save_as):
     fig, ax = plt.subplots(figsize=(9, 3.5))
-    img = ax.imshow(to_plot, cmap='PiYG', norm=norm, origin='lower')
+
+    img = ax.imshow(to_plot, cmap='PiYG', norm=norm,
+                    origin='lower', extent=get_extent(to_plot))
     fig.colorbar(img, ax=ax, orientation='vertical')
     ax.set_ylabel('#High-fid samples')
     ax.set_xlabel('#Low-fid samples')
@@ -120,7 +139,8 @@ def plot_t_scores(data, title, t_range=5, num_colors=10, save_as=None):
     discrete_cmap = plt.get_cmap('PuOr_r', num_colors)
 
     fig, ax = plt.subplots(figsize=(9, 3.5))
-    img = ax.imshow(t_scores, cmap=discrete_cmap, norm=norm, origin='lower')
+    img = ax.imshow(t_scores, cmap=discrete_cmap, norm=norm,
+                    origin='lower', extent=get_extent(data))
     fig.colorbar(img, ax=ax, orientation='vertical', ticks=np.arange(-t_range, t_range, 1))
     ax.set_title(f"Paired difference t-scores - {title}")
 
@@ -134,10 +154,11 @@ def plot_extracts(data, title, save_as=None, show=False):
     fig, ax = plt.subplots(1, 2, figsize=(9, 3.5))
 
     n_highs = data.coords['n_high'].values
+    n_lows = data.coords['n_low'].values
     for nhigh in range(np.min(n_highs), np.max(n_highs) + 1, 10):
         to_plot = data.sel(n_high=nhigh, model='high_hier').median(dim='rep')
-        ax[0].plot(to_plot, label=nhigh)
-        ax[1].plot(to_plot, label=nhigh)
+        ax[0].plot(n_lows, to_plot, label=nhigh)
+        ax[1].plot(n_lows, to_plot, label=nhigh)
 
     ax[0].set_title(title)
     ax[1].set_title(title + ' log-scale')
