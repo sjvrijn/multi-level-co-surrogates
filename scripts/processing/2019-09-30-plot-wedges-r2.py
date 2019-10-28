60f4c8259da9fef0c9915da43c9b188c259600ae
Sander van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2019-10-28 16:18:07 +0100

    removed underscores from filenames; extended parameter input space; added graceful skipping of non-existing files

--- scripts/processing/2019-09-30-plot-wedges-r2.py
+++ scripts/processing/2019-09-30-plot-wedges-r2.py
@@ -26,31 +26,37 @@ data_dir = here("files") / experiment_name
 plot_dir = here("plots") / experiment_name
 plot_dir.mkdir(parents=True, exist_ok=True)
 
+plot_extension = "pdf"
 
 Case = namedtuple('Case', 'name ndim vmin vmax max_diff')
 
 cases = [
     Case(f"AdjustableBranin{a1}", 2, 10, 1e4, 1000)
-    for a1 in np.linspace(-0.5, 1.0, 16)
+    for a1 in np.round(np.linspace(-0.5, 1.0, 31), 2)
 ]
 
 
 for c in cases:
-    with xr.open_dataset(data_dir / f'Matern_{c.ndim}d_{c.name}.nc') as ds:
+    fname = data_dir / f'Matern-{c.ndim}d-{c.name}.nc'
+    if not fname.exists():
+        print(f"Could not find {fname}! Skipping...")
+        continue
+
+    with xr.open_dataset(fname) as ds:
         mses = ds['mses'].load()
 
-    print(mses.coords)
-    print('median')
-    pprint([(f'{95 + i}%-ile', np.nanpercentile(mses.median(dim='rep'), 95 + i)) for i in range(6)])
+    print(fname)
 
     plot_name = f'{c.ndim}d-{c.name}-high-low-samples-linear'
     title = f'{c.name} ({c.ndim}D)'
 
     proc.plot_high_vs_low_num_samples(mses, title, vmin=c.vmin, vmax=c.vmax,
-                                      save_as=plot_dir / f'{plot_name}.pdf')
+                                      save_as=plot_dir / f'{plot_name}.{plot_extension}')
     proc.plot_high_vs_low_num_samples_diff(mses, title, max_diff=c.max_diff,
-                                           save_as=plot_dir / f'{plot_name}_diff.pdf')
+                                           save_as=plot_dir / f'{plot_name}-diff.{plot_extension}')
 
     proc.plot_t_scores(mses, title=title,
-                       save_as=plot_dir / f'{plot_name}_significance.pdf')
-    proc.plot_extracts(mses, title, save_as=plot_dir / f'{plot_name}_extracts.pdf', show=True)
+                       save_as=plot_dir / f'{plot_name}-significance.{plot_extension}')
+    proc.plot_extracts(mses, title,
+                       save_as=plot_dir / f'{plot_name}-extracts.{plot_extension}')
+
