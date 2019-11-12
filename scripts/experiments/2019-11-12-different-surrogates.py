ac788f866eb55a24c0c6b26fd6ff02e6c166ab3d
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2019-11-12 13:40:36 +0100

    Added experiment file for using different surrogates

---
+++ scripts/experiments/2019-11-12-different-surrogates.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+2019-11-12-different-surrogates.py: Experiment runner file for generating data for
+many combinations of numbers of high- vs. low-fidelity samples using different
+surrogate models as backend instead of only using Kriging with Matern Kernel
+"""
+
+__author__ = 'Sander van Rijn'
+__email__ = 's.j.van.rijn@liacs.leidenuniv.nl'
+
+from itertools import product
+
+from pyprojroot import here
+
+import multifidelityfunctions as mff
+
+from experiments import Case, Instance, create_model_error_grid
+
+save_dir = here('files/2019-11-12-different-surrogates/')
+save_dir.mkdir(parents=True, exist_ok=True)
+
+cases = [
+    Case(1, mff.forrester),
+
+    Case(2, mff.forrester),
+    # Case(2, mff.bohachevsky),
+    # Case(2, mff.booth),
+    Case(2, mff.branin),
+    Case(2, mff.currin),
+    # Case(2, mff.himmelblau),
+    # Case(2, mff.six_hump_camelback),
+
+    Case(4, mff.forrester),
+    Case(4, mff.park91a),
+    # Case(4, mff.park91b),
+
+    # Case(6, mff.forrester),
+    Case(6, mff.hartmann6),
+
+    # Case(8, mff.forrester),
+    Case(8, mff.borehole),
+]
+
+surrogate_names = [
+    'RandomForest',
+    'ElasticNet',
+    'SVM'
+]
+
+scaling_options = [
+    'off',
+    # 'on',
+    # 'inverted',
+    # 'regularized'
+]
+
+min_high, max_high = 2, 50
+min_low, max_low = 3, 125
+step = 1
+num_reps = 50
+
+instances = [Instance(h, l, r)
+             for h, l, r in product(range(min_high, max_high + 1, step),
+                                    range(min_low, max_low + 1, step),
+                                    range(num_reps))
+             if h < l]
+
+for case, surr_name, scale in product(cases, surrogate_names, scaling_options):
+    mfbo_options = {'surrogate': surr_name, 'scaling': scale}
+    create_model_error_grid(case, mfbo_options, instances, save_dir=save_dir)
