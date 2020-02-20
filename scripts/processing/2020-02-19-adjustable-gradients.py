171de0fbcb39f5eee85f5afa0d5cf00f555ade7e
Sander van Rijn <svr003@gmail.com>
2020-02-20 17:14:48 +0100

    finished plotting code for adjustable correlation vs gradient

--- scripts/processing/2020-02-19-adjustable-gradients.py
+++ scripts/processing/2020-02-19-adjustable-gradients.py
@@ -7,7 +7,7 @@ gradients in the error versus the actual correlation between high- and low-
 fidelity functions.
 """
 
-from collections import namedtuple
+from collections import defaultdict, namedtuple
 import re
 
 import matplotlib.pyplot as plt
@@ -28,19 +28,36 @@ fname_template = re.compile('[A-Za-z]*-(\d+)d-Adjustable([A-Za-z]*3?)([01].\d+).
 Record = namedtuple('Record', 'experiment file high low rep')
 records = []
 
+correlations = pd.read_csv(here('files') / 'correlations.csv', index_col=0)
+
+to_plot = defaultdict(list)
+
 for file in [f for f in data_dir.iterdir() if f.suffix == '.nc']:
     ndim, func_name, value = fname_template.match(file.name).groups()
+    corrs = correlations.loc[(correlations['name'] == func_name.lower()) & (correlations['param'] == float(value)), 'pearson_r':'spearman_r2'].squeeze()
 
-    # with xr.open_dataset(file) as ds:
-    #     da = ds['mses'].sel(model='high_hier')
-    # with da.load() as da:
-    #     reg = proc.fit_lin_reg(da)
-    #     coef = reg.coef_
+#     with xr.open_dataset(file) as ds:
+#         da = ds['mses'].sel(model='high_hier')
+#     with da.load() as da:
+#         reg = proc.fit_lin_reg(da)
+#     coef = reg.coef_
+#     angle = ...
+
+    angle = np.random.random()*90
+
+    to_plot[func_name].append((corrs['pearson_r'], angle))
 
 
+from pprint import pprint
+pprint(to_plot)
 
-        # records.append(Record(directory.name, file.stem, *coef))
 
-
-df = pd.DataFrame.from_records(records, columns=Record._fields)
-print(df.to_latex())
+for func_name, data in to_plot.items():
+    x, y = zip(*data)
+    plt.scatter(x, y)
+    plt.title(func_name)
+    plt.xlabel('correlation')
+    plt.ylabel('angle')
+    plt.tight_layout()
+    plt.savefig(here('plots') / f'tmp/{func_name}.png')
+    plt.close()
