57ffefaeaa8eb8dc452bd24a9ceff6580b757fab
Sander van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2020-02-25 17:19:12 +0100

    updated gradients of adjustable functions processing functions: correlations vs gradients nicely plotted and intermediate data stored in csv

--- scripts/processing/2020-02-19-adjustable-gradients.py
+++ scripts/processing/2020-02-19-adjustable-gradients.py
@@ -25,39 +25,69 @@ __email__ = 's.j.van.rijn@liacs.leidenuniv.nl'
 data_dir = here("files/2019-10-07-adjustables")
 
 fname_template = re.compile('[A-Za-z]*-(\d+)d-Adjustable([A-Za-z]*3?)([01].\d+).nc')
-Record = namedtuple('Record', 'experiment file high low rep')
+Record = namedtuple('Record', 'fname ndim param pearson_r pearson_r2 spearman_r spearman_r2 alpha beta gamma theta')
 records = []
 
 correlations = pd.read_csv(here('files') / 'correlations.csv', index_col=0)
 
 to_plot = defaultdict(list)
 
-for file in [f for f in data_dir.iterdir() if f.suffix == '.nc']:
-    ndim, func_name, value = fname_template.match(file.name).groups()
-    corrs = correlations.loc[(correlations['name'] == func_name.lower()) & (correlations['param'] == float(value)), 'pearson_r':'spearman_r2'].squeeze()
-
-#     with xr.open_dataset(file) as ds:
-#         da = ds['mses'].sel(model='high_hier')
-#     with da.load() as da:
-#         reg = proc.fit_lin_reg(da)
-#     coef = reg.coef_
-#     angle = ...
-
-    angle = np.random.random()*90
-
-    to_plot[func_name].append((corrs['pearson_r'], angle))
 
 
-from pprint import pprint
-pprint(to_plot)
+
+for file in [f for f in sorted(data_dir.iterdir()) if f.suffix == '.nc']:
+    match = fname_template.match(file.name)
+    if not match:
+        continue
+
+    ndim, func_name, value = match.groups()
+    row = correlations.loc[(correlations['name'] == func_name.lower()) & (correlations['param'] == float(value))].squeeze()
+    corrs = row['pearson_r':'spearman_r2']
+
+    with xr.open_dataset(file) as ds:
+        da = ds['mses'].sel(model='high_hier')
+    with da.load() as da:
+        reg = proc.fit_lin_reg(da)
+    coef = reg.coef_
+    angle = np.arctan2(*reg.coef_[:2])
+    deg_angle = (angle*180 / np.pi) % 180
+    print(coef[:2], angle, deg_angle)
+
+    records.append(Record(*row, *coef, angle))
+
+    to_plot[func_name].append((corrs['pearson_r'], deg_angle))
+
+
+#from pprint import pprint
+#pprint(to_plot)
+
+pd.DataFrame.from_records(records, columns=Record._fields).to_csv(here('files') / 'extended_correlations.csv', index=False)
 
 
 for func_name, data in to_plot.items():
     x, y = zip(*data)
     plt.scatter(x, y)
+    plt.axhline(y=90, alpha=.5, color='black')
     plt.title(func_name)
-    plt.xlabel('correlation')
-    plt.ylabel('angle')
+    plt.xlabel('Correlation $r$')
+    plt.ylabel('Angle of gradient')
     plt.tight_layout()
     plt.savefig(here('plots') / f'tmp/{func_name}.png')
+    plt.savefig(here('plots') / f'tmp/{func_name}.pdf')
     plt.close()
+
+
+for func_name, data in to_plot.items():
+    x, y = zip(*data)
+    plt.scatter(x, y, label=func_name)
+
+
+plt.axhline(y=90, alpha=.5, color='black')
+plt.title("Comparing Adjustable Functions")
+plt.xlabel('Correlation $r$')
+plt.ylabel('Angle of gradient')
+plt.legend(loc=0)
+plt.tight_layout()
+plt.savefig(here('plots') / f'tmp/comparison.png')
+plt.savefig(here('plots') / f'tmp/comparison.pdf')
+plt.close()
