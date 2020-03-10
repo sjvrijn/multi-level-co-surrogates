f16f7121683ab65507c3072787cc0d6f95cae82c
Sander van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2020-03-10 16:20:24 +0100

    updated scatterplot for subsampling gradients: gathered more information in dataframe; improved display with colors, legends, labels, etc

--- scripts/processing/2020-03-09-subsampling-gradients.py
+++ scripts/processing/2020-03-09-subsampling-gradients.py
@@ -29,24 +29,25 @@ subsampling_dir = here("files/2020-03-04-cv-adjustables-subsampling")
 plot_dir = here("plots") / '2020-03-09-subsampling-gradients/'
 plot_dir.mkdir(exist_ok=True, parents=True)
 
-correlations = pd.read_csv(here('files') / 'correlations.csv')
-extended_correlations_path = here('files') / 'extended_correlations.csv'
+correlations = pd.read_csv(here('files') / 'extended_correlations.csv')
 
 
 
-
-fname_template = re.compile(r'[A-Za-z]*-(\d+)d-Adjustable([A-Za-z]*3?)([01].\d+).nc')
-Record = namedtuple('Record', 'orig_deg sub_deg cv_deg')
+fname_template = re.compile(r'[A-Za-z]*-(\d+)d-Adjustable([A-Za-z]*3?)([01].\d+)-sub50-125.nc')
+Record = namedtuple('Record', 'name param pearson_r orig_deg sub_deg cv_deg')
 records = []
-for file in sorted(adjustables_dir.iterdir()):
+names = set()
+for file in sorted(subsampling_dir.iterdir()):
     match = fname_template.match(file.name)
     if not match:
         continue
 
     ndim, func_name, value = match.groups()
-
-    orig_deg = correlations.loc[(correlations['name'] == func_name.lower())
-                                & (correlations['param'] == float(value))]['deg']
+    names.add(func_name)
+    orig_row = correlations.loc[(correlations['fname'] == func_name.lower())
+                                & (correlations['param'] == float(value))]
+    orig_deg = orig_row['deg'].values[0]
+    pearson_r = orig_row['pearson_r'].values[0]
 
     with xr.open_dataset(file) as ds:
         da = ds['mses'].sel(model='high_hier')
@@ -59,21 +60,36 @@ for file in sorted(adjustables_dir.iterdir()):
             reg = proc.fit_lin_reg(da)
         cv_deg = np.rad2deg(np.arctan2(*reg.coef_[:2])) % 180
 
-    records.append(Record(orig_deg, sub_deg, cv_deg))
+    records.append(Record(func_name, value, pearson_r, orig_deg, sub_deg, cv_deg))
 
 df = pd.DataFrame(records)
+print(df)
 
 
+grid_style = dict(b=True, alpha=.5, linestyle=':')
+scatter_style = {'s': 12}
+diag_line = {'linestyle': '--', 'color': 'black', 'alpha':.3, 'linewidth':.5}
 fig, axes = plt.subplots(ncols=2, figsize=(9, 4.5), constrained_layout=True)
 
-axes[0].scatter(df['orig_deg'], df['sub_deg'])
-axes[0].plot([0, 100], '--', color='black', alpha=.3, linewidth=.5)
+
+for name, sub_df in df.groupby('name'):
+    axes[0].scatter(sub_df['orig_deg'], sub_df['sub_deg'], label=name, **scatter_style)
+    axes[1].scatter(sub_df['orig_deg'], sub_df['cv_deg'], label=name, **scatter_style)
+
 axes[0].set_title('Actual gradient vs sub-sampled gradient')
+axes[0].set_ylabel('subsampled gradient')
+axes[0].legend(loc=0)
 
-
-axes[1].scatter(df['orig_deg'], df['cv_deg'])
-axes[1].plot([0, 100], '--', color='black', alpha=.3, linewidth=.5)
 axes[1].set_title('Actual gradient vs cross-validated gradient')
+axes[1].set_ylabel('cross-validated gradient')
+
+for ax in axes:
+    ax.set_xlabel('actual gradient')
+    ax.grid(**grid_style)
+    ax.plot(np.arange(100), **diag_line)
+    ax.set_xlim([0,90])
+    ax.set_ylim([0,90])
 
 plt.savefig(plot_dir / 'scatter_compare.pdf')
 plt.savefig(plot_dir / 'scatter_compare.png')
+
