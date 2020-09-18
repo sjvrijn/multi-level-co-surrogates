1d4012ba0672f0addce7b070ff2b9c5c2ab3acbd
Sander van Rijn <svr003@gmail.com>
2020-09-18 15:05:01 +0200

    refactored adjustable-gradients file; enabled cli arguments to regen csv

--- scripts/processing/2020-02-19-adjustable-gradients.py
+++ scripts/processing/2020-02-19-adjustable-gradients.py
@@ -10,6 +10,7 @@ step to speed up any potential rerun.
 
 from collections import namedtuple
 from itertools import product
+import argparse
 import re
 
 import matplotlib.pyplot as plt
@@ -84,7 +85,7 @@ def calc_angle(da):
     return AngleSummary(alpha, beta, gamma, theta, mid_angle, min(angles), max(angles))
 
 
-def store_extended_correlations():
+def determine_extended_correlations():
 
     Record = namedtuple(
 
@@ -123,76 +124,93 @@ def store_extended_correlations():
                 angle_summary = calc_angle(da)
             records.append(Record(*row, *angle_summary))
 
-    df = pd.DataFrame.from_records(records, columns=Record._fields)
-    return df
+    return pd.DataFrame.from_records(records, columns=Record._fields)
 
 
-if not extended_correlations_path.exists():
-    extended_correlations = store_extended_correlations()
-    extended_correlations.to_csv(extended_correlations_path, index=False)
-else:
-    extended_correlations = pd.read_csv(extended_correlations_path)
+def create_plots(extended_correlations):
+    """plot the error-grid gradient against correlation for the various
+    (adjustable) benchmark functions. Creates plots for both pearson and
+    spearman correlation, and for various groups of functions as well as a
+    single overview plot. Shows estimated upper/lower range of angles based on
+    CI of linear regression fit parameters as error bars."""
+
+    line_at_90 = dict(y=90, alpha=.5, color='black')
+    grid_style = dict(alpha=.5, linestyle=':')
+    xlabel = '{corr_type} Correlation $r$'
+    ylabel = 'Angle of Gradient'
+
+    xticks = np.linspace(-1, 1, 11)
+    correlation_types = ['pearson', 'spearman']
 
 
-line_at_90 = dict(y=90, alpha=.5, color='black')
-grid_style = dict(alpha=.5, linestyle=':')
-xlabel = '{corr_type} Correlation $r$'
-ylabel = 'Angle of Gradient'
+    markers = 'ov^<>+x*1234'
+    for corr_type in correlation_types:
+        for category, df in extended_correlations.groupby('category'):
+            for func_name, sub_df in df.groupby('fname'):
+                x, y = sub_df[f'{corr_type}_r'].values, sub_df['deg'].values
+                errors = np.stack([y - sub_df['deg_low'].values, sub_df['deg_high'].values - y])
+                plt.errorbar(x, y, yerr=errors, ls='', capsize=1)
+                plt.axhline(**line_at_90)
+                plt.title(func_name)
+                plt.xlabel(xlabel)
+                plt.ylabel(ylabel)
+                plt.xticks(xticks)
+                plt.grid(**grid_style)
+                plt.tight_layout()
+                plt.savefig(plot_dir / f'{category}-{func_name}.png')
+                plt.savefig(plot_dir / f'{category}-{func_name}.pdf')
+                plt.close()
 
-xticks = np.linspace(-1, 1, 11)
-correlation_types = ['pearson', 'spearman']
+        fig, ax = plt.subplots(figsize=(7.0,5.2), constrained_layout=True)
 
-
-for corr_type in correlation_types:
-    for category, df in extended_correlations.groupby('category'):
-        for func_name, sub_df in df.groupby('fname'):
+        adjustables = extended_correlations.loc[extended_correlations['category'] == 'adjustable']
+        for func_name, sub_df in adjustables.groupby('fname'):
             x, y = sub_df[f'{corr_type}_r'].values, sub_df['deg'].values
             errors = np.stack([y - sub_df['deg_low'].values, sub_df['deg_high'].values - y])
-            plt.errorbar(x, y, yerr=errors, ls='', capsize=1)
-            plt.axhline(**line_at_90)
-            plt.title(func_name)
-            plt.xlabel(xlabel)
-            plt.ylabel(ylabel)
-            plt.xticks(xticks)
-            plt.grid(**grid_style)
-            plt.tight_layout()
-            plt.savefig(plot_dir / f'{category}-{func_name}.png')
-            plt.savefig(plot_dir / f'{category}-{func_name}.pdf')
-            plt.close()
-
-    fig, ax = plt.subplots(figsize=(7.0,5.2), constrained_layout=True)
-
-    adjustables = extended_correlations.loc[extended_correlations['category'] == 'adjustable']
-    for func_name, sub_df in adjustables.groupby('fname'):
-        x, y = sub_df[f'{corr_type}_r'].values, sub_df['deg'].values
-        errors = np.stack([y - sub_df['deg_low'].values, sub_df['deg_high'].values - y])
-        plt.errorbar(x, y, yerr=errors, capsize=1, linestyle='-', linewidth=.5, marker='.', label=f'adjustable {func_name}')
+            plt.errorbar(x, y, yerr=errors, capsize=1, linestyle='-', linewidth=.5, marker='.', label=f'adjustable {func_name}')
 
 
-    regulars = extended_correlations.loc[extended_correlations['category'] == 'regular']
-    markers = 'ov^<>+x*1234'
-    for (func_name, sub_df), marker in zip(regulars.groupby('fname'), markers):
-        if func_name == 'forrester':
-            sub_df = sub_df.loc[sub_df['ndim'] == 1]
-        x, y = sub_df[f'{corr_type}_r'].values, sub_df['deg'].values
-        errors = np.stack([y - sub_df['deg_low'].values, sub_df['deg_high'].values - y])
-        plt.errorbar(x, y, yerr=errors, ls='', capsize=1, linewidth=.5, marker=marker, label=func_name)
+        regulars = extended_correlations.loc[extended_correlations['category'] == 'regular']
+        for (func_name, sub_df), marker in zip(regulars.groupby('fname'), markers):
+            if func_name == 'forrester':
+                sub_df = sub_df.loc[sub_df['ndim'] == 1]
+            x, y = sub_df[f'{corr_type}_r'].values, sub_df['deg'].values
+            errors = np.stack([y - sub_df['deg_low'].values, sub_df['deg_high'].values - y])
+            plt.errorbar(x, y, yerr=errors, ls='', capsize=1, linewidth=.5, marker=marker, label=func_name)
 
 
-    plt.axhline(**line_at_90)
-    plt.title("Comparing Adjustable Functions")
-    plt.xlabel(xlabel.format(corr_type=corr_type.title()))
-    plt.ylabel(ylabel)
-    plt.xticks(xticks)
-    ax = plt.gca()
-    ax.xaxis.set_minor_locator(MultipleLocator(0.1))
-    ax.yaxis.set_minor_locator(MultipleLocator(10))
-    plt.xlim([-1, 1.05])
-    plt.ylim([0, 120])
-    plt.grid(**grid_style, which='both')
+        plt.axhline(**line_at_90)
+        plt.title("Comparing Adjustable Functions")
+        plt.xlabel(xlabel.format(corr_type=corr_type.title()))
+        plt.ylabel(ylabel)
+        plt.xticks(xticks)
+        ax = plt.gca()
+        ax.xaxis.set_minor_locator(MultipleLocator(0.1))
+        ax.yaxis.set_minor_locator(MultipleLocator(10))
+        plt.xlim([-1, 1.05])
+        plt.ylim([0, 120])
+        plt.grid(**grid_style, which='both')
 
-    plt.legend(loc='lower left')
+        plt.legend(loc='lower left')
 
-    plt.savefig(plot_dir / f'comparison_{corr_type}.png')
-    plt.savefig(plot_dir / f'comparison_{corr_type}.pdf')
-    plt.close()
+        plt.savefig(plot_dir / f'comparison_{corr_type}.png')
+        plt.savefig(plot_dir / f'comparison_{corr_type}.pdf')
+        plt.close()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--regen-csv', action='store_true')
+    parser.add_argument('--no-plots', action='store_false')
+
+    args = parser.parse_args()
+
+    if args.regen_csv or not extended_correlations_path.exists():
+        ext_correlations = determine_extended_correlations()
+        ext_correlations.to_csv(extended_correlations_path, index=False)
+    else:
+        ext_correlations = pd.read_csv(extended_correlations_path)
+
+    if not args.no_plots:
+        create_plots(ext_correlations)
