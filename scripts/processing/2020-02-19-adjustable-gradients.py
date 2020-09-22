39a2a6410d868d1cdbbc96957540c2c77723ac6f
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2020-09-22 14:47:39 +0200

    [wip] removed angle calculation functions from 2020-02-19; rewrote 'script' part of file

--- scripts/processing/2020-02-19-adjustable-gradients.py
+++ scripts/processing/2020-02-19-adjustable-gradients.py
@@ -8,17 +8,13 @@ fidelity functions. Creates a csv of the all relevant values as intermediate
 step to speed up any potential rerun.
 """
 
-from collections import namedtuple
-from itertools import product
 import argparse
 
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
 import numpy as np
 import pandas as pd
-import xarray as xr
 from pyprojroot import here
-from parse import Parser
 
 import processing as proc
 
@@ -35,95 +31,7 @@ correlations_path = here('files/correlations.csv')
 extended_correlations_path = here('files/extended_correlations.csv', warn=False)
 
 
-
-class ConfidenceInterval(namedtuple('ConfidenceInterval', 'mean se lower upper')):
-
-    def __contains__(self, value):
-        return self.lower < value < self.upper
-
-    def __str__(self):
-        lower = self.lower if self.lower is not None else self.mean - 1.96*self.se
-        upper = self.upper if self.upper is not None else self.mean + 1.96*self.se
-        return f'95% CI: {self.mean:.4f} +/- {1.96*self.se:.4f} {np.array([lower, upper])}: H0{" not" if 0 in self else ""} rejected'
-
-
-def ratio_to_angle(x1, x2):
-    theta = np.arctan2(x1, x2) + np.pi
-    deg = np.rad2deg(theta)
-    if deg > 180:
-        deg -= 360  # Example: instead of 350, we want -10
-    return deg
-
-
-def calc_angle(da):
-    AngleSummary = namedtuple('AngleSummary', 'alpha beta theta deg deg_low deg_high')
-    reg, SSE = proc.fit_lin_reg(da, calc_SSE=True)
-
-    alpha, beta = reg.coef_
-    df = da.size - 3
-
-    nhighs = da.coords['n_high'].values
-    var_nhigh = np.sqrt(np.sum((nhighs - np.mean(nhighs))**2))
-
-    nlows = da.coords['n_low'].values
-    var_nlow = np.sqrt(np.sum((nlows - np.mean(nlows))**2))
-
-    s = np.sqrt(SSE / df)
-
-    se_nhigh = s / var_nhigh
-    se_nlow = s / var_nlow
-
-    ci_alpha = ConfidenceInterval(alpha, se_nhigh, alpha-1.96*se_nhigh, alpha+1.96*se_nhigh)
-    ci_beta = ConfidenceInterval(beta, se_nlow, beta-1.96*se_nlow, beta+1.96*se_nlow)
-
-    theta = np.arctan2(alpha, beta) + np.pi
-    mid_angle = np.rad2deg(theta)
-    angles = [ratio_to_angle(a, b) for a, b in product(ci_alpha[2:], ci_beta[2:])]
-
-    return AngleSummary(alpha, beta, theta, mid_angle, min(angles), max(angles))
-
-
-def determine_extended_correlations():
-
-    Record = namedtuple(
-
-        'Record', 'category fname ndim pearson_r pearson_r2 '
-                  'spearman_r spearman_r2 param '
-                  'alpha beta theta deg deg_low deg_high'
-    )
-    records = []
-    correlations = pd.read_csv(correlations_path)
-
-    for category, directory in zip(('regular', 'adjustable'),
-                                   [regulars_dir, adjustables_dir]):
-        if category == 'regular':
-            fname_parser = Parser("{surrogate:w}-{ndim:d}d-{fname}.nc")
-        else:
-            fname_parser = Parser("{surrogate:w}-{ndim:d}d-Adjustable{fname}{param:f}.nc")
-
-        for file in sorted(directory.iterdir()):
-            match = fname_parser.parse(file.name)
-            if not match:
-                continue
-
-            if category == 'regular':
-                row = correlations.loc[(correlations['category'] == 'regular')
-                                       & (correlations['name'] == match['fname'].lower())
-                                       & (correlations['ndim'] == match['ndim'])].squeeze()
-            else:
-                row = correlations.loc[(correlations['name'] == match['fname'].lower())
-                                       & (correlations['param'] == match['param'])].squeeze()
-
-            with xr.open_dataset(file) as ds:
-                da = ds['mses'].sel(model='high_hier')
-            with da.load() as da:
-                angle_summary = calc_angle(da)
-            records.append(Record(*row, *angle_summary))
-
-    return pd.DataFrame.from_records(records, columns=Record._fields)
-
-
-def create_plots(extended_correlations):
+def create_plots(correlations, angles):
     """plot the error-grid gradient against correlation for the various
     (adjustable) benchmark functions. Creates plots for both pearson and
     spearman correlation, and for various groups of functions as well as a
@@ -202,11 +110,16 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if args.regen_csv or not extended_correlations_path.exists():
-        ext_correlations = determine_extended_correlations()
-        ext_correlations.to_csv(extended_correlations_path, index=False)
-    else:
-        ext_correlations = pd.read_csv(extended_correlations_path)
+    if args.regen_csv or not (adjustables_dir / 'gradients.csv').exists():
+        proc.calc_and_store_gradient_angles(adjustables_dir)
+    if args.regen_csv or not (regulars_dir / 'gradients.csv').exists():
+        proc.calc_and_store_gradient_angles(regulars_dir)
+
+    correlations = pd.read_csv(correlations_path, index=False)
+    adjustable_angles = pd.read_csv(adjustables_dir / 'gradients.csv', index=False)
+    regular_angles = pd.read_csv(regulars_dir / 'gradients.csv', index=False)
+
+    angles = pd.concat([regular_angles, adjustable_angles], ignore_index=True)
 
     if not args.no_plots:
-        create_plots(ext_correlations)
+        create_plots(correlations, angles)
