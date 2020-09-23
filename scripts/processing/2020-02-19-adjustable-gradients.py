343293baeb4a2edc79fbf01bf7d1426d2f625dad
Sander van Rijn <svr003@gmail.com>
2020-09-23 16:23:12 +0200

    Scatterplotting each separate function moved to separate (optional) function

--- scripts/processing/2020-02-19-adjustable-gradients.py
+++ scripts/processing/2020-02-19-adjustable-gradients.py
@@ -30,43 +30,30 @@ plot_dir.mkdir(exist_ok=True, parents=True)
 correlations_path = here('files/correlations.csv')
 extended_correlations_path = here('files/extended_correlations.csv', warn=False)
 
+line_at_90 = dict(y=90, alpha=.5, color='black')
+grid_style = dict(alpha=.5, linestyle=':')
+xlabel = '{corr_type} Correlation $r$'
+ylabel = 'Angle of Gradient'
+correlation_range = np.linspace(-1, 1, 11)
 
-def create_plots(correlations, angles):
+
+def create_plots(correlations, angles, plot_individuals=False):
     """plot the error-grid gradient against correlation for the various
     (adjustable) benchmark functions. Creates plots for both pearson and
     spearman correlation, and for various groups of functions as well as a
     single overview plot. Shows estimated upper/lower range of angles based on
     CI of linear regression fit parameters as error bars."""
 
-    line_at_90 = dict(y=90, alpha=.5, color='black')
-    grid_style = dict(alpha=.5, linestyle=':')
-    xlabel = '{corr_type} Correlation $r$'
-    ylabel = 'Angle of Gradient'
-
-    xticks = np.linspace(-1, 1, 11)
     correlation_types = ['pearson', 'spearman']
 
     extended_correlations = angles.merge(correlations)
 
     markers = 'ov^<>+x*1234'
     for corr_type in correlation_types:
-        for category, df in extended_correlations.groupby('category'):
-            for func_name, sub_df in df.groupby('fname'):
-                x, y = sub_df[f'{corr_type}_r'].values, sub_df['deg'].values
-                errors = np.stack([y - sub_df['deg_low'].values, sub_df['deg_high'].values - y])
-                plt.errorbar(x, y, yerr=errors, ls='', capsize=1)
-                plt.axhline(**line_at_90)
-                plt.title(func_name)
-                plt.xlabel(xlabel)
-                plt.ylabel(ylabel)
-                plt.xticks(xticks)
-                plt.grid(**grid_style)
-                plt.tight_layout()
-                plt.savefig(plot_dir / f'{category}-{func_name}.png')
-                plt.savefig(plot_dir / f'{category}-{func_name}.pdf')
-                plt.close()
+        if plot_individuals:
+            scatter_per_function(corr_type, extended_correlations)
 
-        fig, ax = plt.subplots(figsize=(7.0,5.2), constrained_layout=True)
+        plt.subplots(figsize=(7.0,5.2), constrained_layout=True)
 
         adjustables = extended_correlations.loc[extended_correlations['category'] == 'adjustable']
         for func_name, sub_df in adjustables.groupby('fname'):
@@ -86,7 +73,7 @@ def create_plots(correlations, angles):
         plt.title("Comparing Adjustable Functions")
         plt.xlabel(xlabel.format(corr_type=corr_type.title()))
         plt.ylabel(ylabel)
-        plt.xticks(xticks)
+        plt.xticks(correlation_range)
         ax = plt.gca()
         ax.xaxis.set_minor_locator(MultipleLocator(0.1))
         ax.yaxis.set_minor_locator(MultipleLocator(10))
@@ -99,6 +86,24 @@ def create_plots(correlations, angles):
         plt.savefig(plot_dir / f'comparison_{corr_type}.png')
         plt.savefig(plot_dir / f'comparison_{corr_type}.pdf')
         plt.close()
+
+
+def scatter_per_function(corr_type, extended_correlations):
+    for category, df in extended_correlations.groupby('category'):
+        for func_name, sub_df in df.groupby('fname'):
+            x, y = sub_df[f'{corr_type}_r'].values, sub_df['deg'].values
+            errors = np.stack([y - sub_df['deg_low'].values, sub_df['deg_high'].values - y])
+            plt.errorbar(x, y, yerr=errors, ls='', capsize=1)
+            plt.axhline(**line_at_90)
+            plt.title(func_name)
+            plt.xlabel(xlabel)
+            plt.ylabel(ylabel)
+            plt.xticks(correlation_range)
+            plt.grid(**grid_style)
+            plt.tight_layout()
+            plt.savefig(plot_dir / f'{category}-{func_name}-{corr_type}.png')
+            plt.savefig(plot_dir / f'{category}-{func_name}-{corr_type}.pdf')
+            plt.close()
 
 
 if __name__ == '__main__':
