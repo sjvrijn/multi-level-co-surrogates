79e9117f35b8e470a96d540eb3e0affb6f5ff2a2
Sander van Rijn <svr003@gmail.com>
2020-09-28 23:17:42 +0200

    Added boilerplate for shape reduction experiment/processing

---
+++ scripts/processing/2020-09-28-shape-reduction.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+2020-09-28-shape-reduction.py: Investigating if only taking part of a fully
+enumerated Error Grid can still result in similar gradients, i.e.: what is the
+minimum Error Grid data to compute?.
+"""
+
+import argparse
+from itertools import product
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+from pyprojroot import here
+
+import processing as proc
+
+
+kriging_path = here('files/2019-09-mse-nc/')
+
+
+
+def extract_right_upper_square(da: xr.DataArray, num_high, num_low) -> xr.DataArray:
+    pass
+
+
+def extract_right_upper_triangle(da: xr.DataArray, num_high, num_low) -> xr.DataArray:
+    pass
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    args = parser.parse_args()
+
