01e2acedd7ee8c225553ec5b7a720d0ff9e2f56e
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2020-09-29 17:09:22 +0200

    added initial experiment loop over files and 'shapes'; simplified extract_at_interval to single argument

--- scripts/processing/2020-09-28-shape-reduction.py
+++ scripts/processing/2020-09-28-shape-reduction.py
@@ -26,18 +26,31 @@ def extract_right_upper_square(da: xr.DataArray, num_high, num_low) -> xr.DataAr
     return da.sel(n_high=slice(-num_high, None), n_low=slice(-num_low, None))
 
 
-def extract_at_interval(da: xr.DataArray, num_high_interval, num_low_interval) -> xr.DataArray:
+def extract_at_interval(da: xr.DataArray, interval) -> xr.DataArray:
     return da.sel(
-        n_high=slice(None, None, num_high_interval),
-        n_low=slice(None, None, num_low_interval)
+        n_high=slice(None, None, interval),
+        n_low=slice(None, None, interval)
     )
 
 
 def extract_right_upper_triangle(da: xr.DataArray, num_high, num_low) -> xr.DataArray:
-    pass
+    raise NotImplementedError
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     args = parser.parse_args()
 
+    square_sizes = [(2*size, 5*size) for size in range(1, 21)]
+    intervals = range(1, 11)
+
+    for file in filter(lambda x: '.nc' in x.name, kriging_path.iterdir()):
+        orig_da = xr.open_dataset(file)['mses'].load()
+
+        for square_size, interval in product(square_sizes, intervals):
+            smaller_da = extract_right_upper_square(orig_da, 10, 20)
+            smaller_da = extract_at_interval(smaller_da, interval)
+
+            angle = proc.calc_angle(smaller_da)
+            #TODO: do storage
+            #TODO: do plotting
