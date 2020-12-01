820d9b9ea33bf2acaffa69e8de8b2fa9fa6670ce
Sander van Rijn <svr003@gmail.com>
2020-12-01 23:01:30 +0100

    Added simple fixed ratio mfbo: tau=1/cost_ratio

--- scripts/experiments/2020-11-05-simple-mfbo.py
+++ scripts/experiments/2020-11-05-simple-mfbo.py
@@ -140,6 +140,96 @@ def simple_multifid_bo(func, budget, cost_ratio, doe_n_high, doe_n_low, num_reps
     return mfbo, pd.DataFrame.from_records(entries, columns=Entry._fields), archive
 
 
+def fixed_ratio_multifid_bo(func, budget, cost_ratio, doe_n_high, doe_n_low, num_reps=50):
+    np.random.seed(20160501)
+
+    if doe_n_high + cost_ratio*doe_n_low >= budget:
+        raise ValueError('Budget should not be exhausted after DoE')
+
+    Entry = namedtuple('Entry', 'budget time_since_high_eval tau fidelity candidate fitness')
+    entries = []
+
+    tau = 1 / cost_ratio
+
+    #make mf-DoE
+    high_x, low_x = bi_fidelity_doe(func.ndim, doe_n_high, doe_n_low)
+    high_x, low_x = scale_to_function(func, [high_x, low_x])
+    high_y, low_y = func.high(high_x), \
+                    func.low(low_x)
+
+    #subtract mf-DoE from budget
+    budget -= doe_n_high
+    budget -= doe_n_low * cost_ratio
+
+    #create archive
+    archive = mlcs.CandidateArchive.from_multi_fidelity_function(func, ndim=func.ndim)
+    archive.addcandidates(low_x, low_y, fidelity='low')
+    archive.addcandidates(high_x, high_y, fidelity='high')
+
+    #make mf-model using archive
+    mfbo = mlcs.MultiFidelityBO(func, archive)
+
+    time_since_high_eval = 0
+    while budget > 0:
+        #select next fidelity to evaluate:
+        #compare \tau with current count t to select fidelity, must be >= 1
+        fidelity = 'high' if 1 <= tau <= time_since_high_eval else 'low'
+
+        #predict best place to evaluate:
+        if fidelity == 'high':
+            #best predicted low-fid only datapoint for high-fid (to maintain hierarchical model)
+            all_low = {
+                tuple(candidate)
+                for candidate in archive.getcandidates(fidelity='low').candidates
+            }
+
+            all_high = {
+                tuple(candidate)
+                for candidate in archive.getcandidates(fidelity='high').candidates
+            }
+
+            selected_candidates = all_low - all_high
+
+            candidates = [np.array(cand).reshape(1, -1) for cand in selected_candidates]  # only consider candidates that are not yet evaluated in high-fidelity
+            candidate_predictions = [
+                (cand, mfbo.models['high'].predict(cand.reshape(1, -1)))
+                for cand in candidates
+            ]
+
+            x = min(candidate_predictions, key=itemgetter(1))[0].ravel()
+            time_since_high_eval = 0
+            budget -= 1
+        else:  # elif fidelity == 'low':
+            #simple optimization for low-fid
+            x = minimize(
+                lambda x: mfbo.models['high'].predict(x.reshape(1, -1)),
+                x0=np.random.uniform(func.l_bound, func.u_bound).reshape(-1, ),
+                bounds=func.bounds.T,
+            ).x
+
+            N_RAND_SAMPLES = 100
+            while x in archive:  # resample to ensure a new candidate is added to the archive
+                print(f'Existing candidate {x} ...')
+                random_candidates = scale_to_function(func, np.random.rand(N_RAND_SAMPLES, func.ndim))
+                fitnesses = mfbo.models['high'].predict(random_candidates)
+                x = random_candidates[np.argmin(fitnesses)]
+                print(f'... replaced by {x}')
+
+            time_since_high_eval += 1
+            budget -= cost_ratio
+
+        #evaluate best place
+        y = func[fidelity](x.reshape(1, -1))[0]
+        archive.addcandidate(x, y, fidelity=fidelity)
+        entries.append(Entry(budget, time_since_high_eval, tau, fidelity, x, y))
+
+        #update model
+        mfbo.retrain()
+
+    return mfbo, pd.DataFrame.from_records(entries, columns=Entry._fields), archive
+
+
+
 if __name__ == '__main__':
     import mf2
     from pickle import dump
@@ -166,4 +256,16 @@ if __name__ == '__main__':
         )
         df.to_csv(save_dir / f'{func.name}-tracking.csv')
         with open(save_dir / f'{func.name}-archive.pkl', 'wb') as f:
+            dump(str(archive.data), f)
+
+        _, df, archive = fixed_ratio_multifid_bo(
+            func=func,
+            budget=50,
+            cost_ratio=0.2,
+            doe_n_high=10,
+            doe_n_low=25,
+            num_reps=20
+        )
+        df.to_csv(save_dir / f'{func.name}-tracking-fixed.csv')
+        with open(save_dir / f'{func.name}-archive-fixed.pkl', 'wb') as f:
             dump(str(archive.data), f)
