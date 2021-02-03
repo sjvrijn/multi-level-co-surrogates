32586fb83c6941466e10fa5c3b44f6995830f27f
Sander van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2021-02-03 22:59:23 +0100

    updated bi-fid-doe illustration: added 'all arrows' option

--- scripts/processing/illustrated-bi-fid-doe.py
+++ scripts/processing/illustrated-bi-fid-doe.py
@@ -17,14 +17,22 @@ def low_lhs_sample(ndim, nlow):
         return lhs(ndim, nlow)
 
 
-def illustrated_bi_fidelity_doe(ndim, num_high, num_low, intermediate=True,
-                                as_pdf=True, save_dir=None):
+def illustrated_bi_fidelity_doe(
+    ndim, num_high, num_low, *,
+    intermediate=True,
+    as_pdf=True,
+    save_dir=None,
+    show=False,
+):
     """Create a Design of Experiments (DoE) for two fidelities in `ndim`
     dimensions. The high-fidelity samples are guaranteed to be a subset
     of the low-fidelity samples.
 
     :returns high-fidelity samples, low-fidelity samples
     """
+
+    if not (show or save_dir):
+        return # No output needed, why do any work?
 
     extension = 'pdf' if as_pdf else 'png'
 
@@ -45,6 +53,26 @@ def illustrated_bi_fidelity_doe(ndim, num_high, num_low, intermediate=True,
         'length_includes_head': True,
     }
 
+    if not intermediate:
+        # Plot initial setup, arrows are plotted later on-the-fly
+        xlow = low_x.T[0]
+        xhigh = high_x.T[0]
+        if ndim >= 2:
+            ylow = low_x.T[1]
+            yhigh = high_x.T[1]
+        else:
+            ylow = np.zeros(xlow.shape)
+            yhigh = np.zeros(xhigh.shape)
+
+        plt.figure(figsize=fig_size, constrained_layout=True)
+        plt.scatter(xlow, ylow, label='low', **low_style)
+        plt.scatter(xhigh, yhigh, label='high', **high_style)
+        plt.xticks([])
+        plt.yticks([])
+        plt.xlabel('$x_1$')
+        plt.ylabel('$x_2$')
+        plt.title(f'start')
+
     #TODO: this is the naive method, potentially speed up?
     highs_to_match = set(range(num_high))
     while highs_to_match:
@@ -53,6 +81,7 @@ def illustrated_bi_fidelity_doe(ndim, num_high, num_low, intermediate=True,
         high_idx, low_idx = np.argwhere(dists == min_dist)[0]
 
         if intermediate:
+            # Plot updated DoE with arrow of next point to be moved
             xlow = low_x.T[0]
             xhigh = high_x.T[0]
             if ndim >= 2:
@@ -77,8 +106,17 @@ def illustrated_bi_fidelity_doe(ndim, num_high, num_low, intermediate=True,
             plt.title(f'step {num_high-len(highs_to_match)}/{num_high}')
             if save_dir:
                 plt.savefig(save_dir / f'illustrated-bi-fid-doe-{num_high-len(highs_to_match)}.{extension}')
-            plt.show()
+            if show:
+                plt.show()
             plt.close()
+        else:
+            # Just add the arrow to the 'global' plot
+            plt.arrow(
+                *low_x[low_idx],
+                *(high_x[high_idx] - low_x[low_idx]),
+                **arrow_style,
+            )
+
 
         low_x[low_idx] = high_x[high_idx]
         # make sure just selected samples are not re-selectable
@@ -86,6 +124,12 @@ def illustrated_bi_fidelity_doe(ndim, num_high, num_low, intermediate=True,
         dists[:,low_idx] = np.inf
         highs_to_match.remove(high_idx)
 
+    if not intermediate:
+        if save_dir:
+            plt.savefig(save_dir / f'illustrated-bi-fid-doe-start.{extension}')
+        if show:
+            plt.show()
+        plt.close()
 
     xlow = low_x.T[0]
     xhigh = high_x.T[0]
@@ -103,17 +147,28 @@ def illustrated_bi_fidelity_doe(ndim, num_high, num_low, intermediate=True,
     plt.yticks([])
     plt.xlabel('$x_1$')
     plt.ylabel('$x_2$')
-    plt.title(f'step {num_high-len(highs_to_match)}/{num_high}')
+
+    if not intermediate:
+        plot_title = 'end'
+        save_title = f'illustrated-bi-fid-doe-end.{extension}'
+    else:
+        plot_title = f'step {num_high-len(highs_to_match)}/{num_high}'
+        save_title = f'illustrated-bi-fid-doe-{ndim}d-{num_high}-{num_low}-{num_high-len(highs_to_match)}.{extension}'
+
+    plt.title(plot_title)
     if save_dir:
-        plt.savefig(save_dir / f'illustrated-bi-fid-doe-{ndim}d-{num_high}-{num_low}-{num_high-len(highs_to_match)}.{extension}')
-    plt.show()
+        plt.savefig(save_dir / save_title)
+    if show:
+        plt.show()
 
     return BiFidelityDoE(high_x, low_x)
 
 
-np.random.seed(20160501)
 
 plot_dir = here('plots') / 'illustrated-doe'
 plot_dir.mkdir(exist_ok=True, parents=True)
 
+np.random.seed(20160501)
 _ = illustrated_bi_fidelity_doe(2, 10, 20, save_dir=plot_dir)
+np.random.seed(20160501)
+_ = illustrated_bi_fidelity_doe(2, 10, 20, save_dir=plot_dir, intermediate=False)
