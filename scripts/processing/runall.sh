e8d9fc9f7a7482de02497f9557c71567333ea158
Sander van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2021-02-18 23:32:11 +0100

    Disable default plotting of regular's subsampling
    Since the main focus on the paper is on analysing the adjustable
    functions, no subsampling figures are included in the paper and
    most possible files are not generated either.
    
    This means that for these plot scripts to make sense, the associated
    cases should be enabled in the experiment scripts and run.

--- scripts/processing/runall.sh
+++ scripts/processing/runall.sh
@@ -4,9 +4,9 @@ python3 ./2019-10-15-plot-error-grids-adjustables.py && \
 python3 ./2019-10-30-correlation-table.py && \
 #python3 ./2019-11-05-extended-extracts.py && \
 #python3 ./2019-11-18-plot-different-surrogates.py && \
-python3 ./2019-11-25-subsampling.py && \
+#python3 ./2019-11-25-subsampling.py && \
 python3 ./2019-11-27-gradients.py && \
-python3 ./2020-02-18-combined-error-grids.py && \
+#python3 ./2020-02-18-combined-error-grids.py && \
 python3 ./2020-02-19-adjustable-gradients.py && \
 python3 ./2020-03-09-adjustables-subsampling-comparisons.py && \
 python3 ./2020-03-09-subsampling-gradients.py && \
