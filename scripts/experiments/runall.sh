b77e5f7dcb28c3ffd702fba053ef8e470c66fbcb
Sander van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2021-02-18 17:13:44 +0100

    disabled unused experiments in runall.sh

--- scripts/experiments/runall.sh
+++ scripts/experiments/runall.sh
@@ -1,10 +1,10 @@
 python3 ./2019-09-01-error-grids.py && \
-python3 ./2019-09-10-plot-specifics.py && \
-python3 ./2019-09-13-xarray-test.py && \
+#python3 ./2019-09-10-plot-specifics.py && \
+#python3 ./2019-09-13-xarray-test.py && \
 python3 ./2019-10-07-adjustables.py && \
-python3 ./2019-11-01-extend-ratios.py && \
-python3 ./2019-11-12-different-surrogates.py && \
+#python3 ./2019-11-01-extend-ratios.py && \
+#python3 ./2019-11-12-different-surrogates.py && \
 # python3 ./2019-11-15-bootstrap-resampling.py && \
-python3 ./2020-02-25-illustrating-1dforrester.py && \
+#python3 ./2020-02-25-illustrating-1dforrester.py && \
 python3 ./2020-03-03-cv-bootstrap-resampling.py && \
 python3 ./2020-03-04-cv-adjustables-resampling.py
