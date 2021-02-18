776a8a835adc037a9d44e3575fb5b39255b08a9f
Sander van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2021-02-18 17:01:52 +0100

    renamed illustrated-bi-fid-doe to include date

--- scripts/processing/runall.sh
+++ scripts/processing/runall.sh
@@ -12,5 +12,5 @@ python3 ./2020-03-09-adjustables-subsampling-comparisons.py && \
 python3 ./2020-03-09-subsampling-gradients.py && \
 python3 ./2020-07-06-extrapolation.py && \
 python3 ./2020-07-07-intercept-illustration.py && \
+python3 ./2020-07-29-illustrated-bi-fid-doe.py
 #python3 ./plot-linreg-example.py && \
-python3 ./illustrated-bi-fid-doe.py
