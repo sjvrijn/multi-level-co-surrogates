28ab83402fb395fcbd0b2db831166c51f9a2f75e
Sander van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2021-04-02 16:50:47 +0200

    Add calculate_reuse_fraction method
    This encodes the simple derived forms for how many subsamples
    can be re-used, including semi-extensive documentation stating
    the motivation behind the calculation

--- scripts/experiments/protoEG.py
+++ scripts/experiments/protoEG.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from collections import defaultdict, namedtuple
 from dataclasses import dataclass
 from itertools import product
 from warnings import warn
@@ -13,7 +13,6 @@ class InstanceSpec:
     min_low:   int = 3
     max_high:  int
     max_low:   int
-    step:      int = 1
     step_high: int = None
     step_low:  int = None
     num_reps:  int = 50
@@ -61,11 +60,125 @@ class InstanceSpec:
 
 class ProtoEG:
 
-    def __init__(self):
-        """Container for everything needed to create (advanced) Error Grids
+    def __init__(self, archive: CandidateArchive):
+        """Container for everything needed to create (advanced) Error Grids"""
 
-        Specifically contains everything needed to
+        self.archive = archive
+        self.models = defaultdict(list)  # models[(n_high, n_low)] = [model_1, ..., model_nreps]
+        self.error_grid = None  # xr.DataArray
+
+
+    def add_new_sample(self, X, y: float, fidelity: str):
+        """Add a new sample of given fidelity and update Error Grid accordingly"""
+
+        #Add (X, y) to archive
+        #
+        #
+        #for pixel in error grid:
+        #
+        #    fraction = 1 - calculate_resample_fraction(h, l, fidelity)
+        #
+        #    num_models_to_resample = fraction * num_reps
+        #    uniform at random select num_models_to_resample from num_reps
+        #
+        #    for each model_to_resample:
+        #        create new subsample consisting of at least latest addition X
+        #        create and store model
+        #        calculate and store error
+        #
+        #    if fidelity == 'low':
+        #        # Error values of remaining models remains unchanged
+        #    elif fidelity == 'high':
+        #        for each model_not_resampled:
+        #            add (X, y) to test-set for that model
+        #            recalculate error with new test-set
+        #
+        #return updated errorgrid (?)
+        raise NotImplementedError()
+
+
+    def calculate_reuse_fraction(self, num_high: int, num_low: int, fidelity: str,
+                                 *, max_high: int=None, max_low: int=None) -> float:
+        """Calculate the fraction of models that can be reused
+
+        Given `max_high` H, `max_low` L, `num_high` h and `num_low` l, the number of
+        unique possible subsamples is given by binom(H, h) * binom(L-h, l-h), i.e.:
+
+        /H\/L-h\
+        \h/\l-h/
+
+        In the iterative case when samples are added to H or L one at a time,
+        it can be expected that some of the subsamples would not use the new
+        samples, and therefore that a fraction of previous subsamples can be
+        reused when calculating the Error Grid for the next iteration.
+
+        Assuming subsampling is done uniformly at random, the fraction of
+        subsamples in the 'next' iteration that only use samples from the
+        previous iteration is equal to the ratio between the numbers of total
+        possible subsamples for those given sizes, i.e.:
+
+        /H\/L-h\
+        \h/\l-h/
+        ___________
+
+        /H+1\/L-h\
+        \ h /\l-h/
+
+        if H := H+1, or
+
+
+        /H\/L-h\
+        \h/\l-h/
+        ___________
+
+        /H\/L+1-h\
+        \h/\ l-h /
+
+        if L := L+1
+
+        :param num_high: number of high-fidelity samples in the subsample
+        :param num_low: number of low-fidelity samples in the subsample
+        :param fidelity: fidelity in which the latest sample has been added
+        :param max_high: total number of high-fidelity samples, taken from self.archive if not given
+        :param max_low: total number of low-fidelity samples, taken from self.archive if not given
+        :returns: fraction [0, 1] of samples that can be reused
         """
-        pass
+
+        max_high = len(self.archive.getcandidates(fidelity='high')) if not max_high else max_high
+        max_low = len(self.archive.getcandidates(fidelity='low')) if not max_low else max_low
+
+        if fidelity == 'high':
+            return (max_high-num_high+1) / (max_high+1)
+
+        if fidelity == 'low':
+            return (max_low-num_low+1) / (max_low-num_high+1)
+
+        raise ValueError(f'Invalid fidelity `{fidelity}` given, expected `high` or `low`.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
