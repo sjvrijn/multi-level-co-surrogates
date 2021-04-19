7147dc725cc7e02baac535c04a558edbbd2c90d0
Sander van Rijn <svr003@gmail.com>
2021-04-19 20:35:13 +0200

    extracted pure multi-fidelity-model from MultiFidBO

---
+++ multiLevelCoSurrogates/surrogates/multi_fidelity_model.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+Class file for a multi-fidelity model based on hierarchical surrogate models
+"""
+
+from more_itertools import stagger
+
+from multiLevelCoSurrogates import CandidateArchive
+from .Surrogates import HierarchicalSurrogate, Surrogate
+
+
+class MultiFidelityModel:
+
+    def __init__(
+            self, fidelities, archive: CandidateArchive=None, *,
+            normalized=True, surrogate_name='Kriging', kernel=None, scaling='on'
+    ):
+        """A complete multi-fidelity model consisting of an arbitrary amount of
+         stacked hierarchical surrogate models with a regular surrogate model
+         down at the bottom of the stack.
+
+        :param fidelities:      List of fidelity names, from highest to lowest
+        :param archive:         Archive containing all samples to train the models on
+        :param normalized:
+        :param surrogate_name:
+        :param kernel:
+        :param scaling:
+        """
+        self.fidelities = fidelities
+        self.archive = archive
+        self.normalized = normalized
+
+        kwargs = {
+            'surrogate_name': surrogate_name,
+            'candidate_archive': self.archive,
+            'normalized': normalized,
+            'kernel': kernel
+        }
+
+        self.models = {
+            fidelities[-1]: Surrogate.fromname(**kwargs, fidelity=fidelities[-1])
+        }
+
+        for fid_low, fid_high in stagger(reversed(self.fidelities), offsets=(0, 1)):
+            model = HierarchicalSurrogate(
+                **kwargs,
+                fidelities=[fid_high, fid_low],
+                lower_fidelity_model=self.models[fid_low],
+                scaling=scaling,
+            )
+            self.models[fid_high] = model
+
+        self.top_level_model = self.models[self.fidelities[0]]
+        self.retrain()
+
+
+    def retrain(self):
+        self.top_level_model.retrain()
---
+++ tests/test_multi_fidelity_model.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+test_multi_fidelity_model.py: Set of tests for the mlcs.surrogate.MultiFidelityModel
+"""
+
+__author__ = 'Sander van Rijn'
+__email__ = 's.j.van.rijn@liacs.leidenuniv.nl'
+
+
+import numpy as np
+import mf2
+import multiLevelCoSurrogates as mlcs
+
+
+def test_simple_path():
+    """Quick test case recreating the 1D forrester model"""
+
+    np.random.seed(20160501)  # unbiased random seed
+
+    low_x = np.linspace(0,1,11).reshape(-1,1)
+    high_x = low_x[[0,4,6,10]]
+    archive = mlcs.CandidateArchive(1, ['high', 'low', 'high-low'])
+    archive.addcandidates(low_x, mf2.forrester.low(low_x), fidelity='low')
+    archive.addcandidates(high_x, mf2.forrester.high(high_x), fidelity='high')
+    mfm = mlcs.MultiFidelityModel(mf2.forrester.fidelity_names, archive)
+
+    test_x = high_x + 0.05
+    test_y = mfm.top_level_model.predict(test_x)
+
+    expected = [1.2319664471068705, 1.9409778220340197, -4.0374255643707695, 18.010376232109593]
+
+    np.testing.assert_almost_equal(test_y, expected, decimal=5)
+
