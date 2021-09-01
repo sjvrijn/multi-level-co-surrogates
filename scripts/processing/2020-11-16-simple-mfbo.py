1efa5b1101ad57e17d24f99464d01b57bafedf3b
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2021-09-01 17:24:54 +0200

    log-plotting rewritten to deal with new folder structure

--- scripts/processing/2020-11-16-simple-mfbo.py
+++ scripts/processing/2020-11-16-simple-mfbo.py
@@ -6,41 +6,18 @@
 files during the simple-mfbo runs
 """
 
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import mf2
 from pathlib import Path
 from parse import compile
 from pyprojroot import here
-
 
 
 data_path = here('files/2020-11-05-simple-mfbo/')
 plot_path = here('plots/2020-11-16-simple-mfbo/', warn=False)
 plot_path.mkdir(exist_ok=True, parents=True)
 
-archive_template = compile('{name}-archive-{method}-b{budget:d}-i{idx:d}.pkl')
-tracking_template = compile('{name}-tracking-{method}-b{budget:d}-i{idx:d}.csv')
-
-
-def plot_tracking_for_file(tracking_file: Path):
-    if not tracking_file.exists():
-        return
-    match = tracking_template.parse(tracking_file.name)
-    if not match:
-        return
-
-    fig, axes = plt.subplots(ncols=2, nrows=2, figsize=(8, 6), constrained_layout=True)
-    fig.suptitle(f"{match['method']} for {match['name']} with budget={match['budget']} (idx {match['idx']})")
-
-    df = pd.read_csv(tracking_file, index_col=0)
-    budget_used = match['budget'] - df['budget'].values
-
-    plot_on_axes(axes, budget_used, df)
-
-    # fig.show()
-    fig.savefig(plot_path / tracking_file.with_suffix('.png').name)
+subfolder_template = compile('{func_name}-{method}-b{budget:d}-i{idx:d}')
 
 
 def plot_on_axes(axes, budget_used, df, label=''):
@@ -93,98 +70,74 @@ def plot_comparison(file_a: Path, file_b: Path):
     fig.savefig(plot_path / file_a.with_suffix('.png').name.replace('naive', 'comparison'))
 
 
-def plot_old_file_formats():
-    # ## Plotting progress of $\tau$ over time
-
-    for func in mf2.bi_fidelity_functions:
-        tracking_file = data_path.joinpath(f'{func.name}-tracking.csv')
-        if tracking_file.exists():
-            print(func.name)
-            df = pd.read_csv(tracking_file, index_col=0)
-            df['tau'].plot()
-            plt.savefig(plot_path / f'{func.name}-tau.png', bbox_inches='tight')
-            #plt.show()
-            plt.close()
-
-    # ## Plotting best high/low-fidelity evaluation over time
-    # ### New-style: using `df[fitness]`
-
-    for func in mf2.bi_fidelity_functions:
-        tracking_file = data_path.joinpath(f'{func.name}-tracking.csv')
-        if tracking_file.exists():
-            print(func.name)
-            df = pd.read_csv(tracking_file, index_col=0)
-            #print(df.head())
-            #print(df.dtypes)
-            for name, sub_df in df.groupby('fidelity'):
-                time = sub_df['budget'].to_numpy().reshape(-1,)
-                #fitnesses = sub_df['fitness'].to_numpy().reshape(-1,)
-                fitnesses = [eval(x)[0] for x in sub_df['fitness']]
-                min_fit = np.minimum.accumulate(fitnesses)
-
-                plt.plot(time, fitnesses, label=f'{name}-fidelity over time')
-                plt.plot(time, min_fit, label=f'best {name}-fidelity over time')
-            plt.legend(loc=0)
-            plt.xlim([35, 0])
-            plt.savefig(plot_path / f'{func.name}-evals.png', bbox_inches='tight')
-            #plt.show()
-            plt.close()
-
-
 def make_all_plots_for_experiment_directory(experiment_dir: Path):
 
-    df = pd.read_csv(experiment_dir / 'log.csv', index_col=0)
-    plot_from_log(df)
+    # plot_title = f'{func.ndim}D {func.name} with {budget:.1f} budget left'
+    # proto_eg.plot_errorgrid(
+    #     title=plot_title,
+    #     as_log=True,
+    #     save_as=plot_dir / f'protoeg-EG-opt-{func.name}-{budget/cost_ratio:.0f}',
+    #     save_exts=('png',),
+    #     xlim=(3, init_budget),
+    #     ylim=(2, (init_budget // 2)),
+    # )
+    # try:
+    #     plot_archive(
+    #         archive,
+    #         func,
+    #         title=plot_title,
+    #         save_as=plot_dir / f'protoeg-archive-opt-{func.name}-{budget/cost_ratio:.0f}',
+    #         save_exts=('png',),
+    #     )
+    # except NotImplementedError:
+    #     pass
+    ...
 
 
+def plot_log(in_folder: Path, out_folder: Path, extensions=('png',)):
+    match = subfolder_template.parse(in_folder.name)
+    if not match:
+        return
 
+    fig, axes = plt.subplots(ncols=2, nrows=2, figsize=(8, 6), constrained_layout=True)
+    fig.suptitle(
+        f"{match['method']} for {match['func_name']} with budget={match['budget']} (idx {match['idx']})")
 
-
-    plot_title = f'{func.ndim}D {func.name} with {budget:.1f} budget left'
-    proto_eg.plot_errorgrid(
-        title=plot_title,
-        as_log=True,
-        save_as=plot_dir / f'protoeg-EG-opt-{func.name}-{budget/cost_ratio:.0f}',
-        save_exts=('png',),
-        xlim=(3, init_budget),
-        ylim=(2, (init_budget // 2)),
-    )
+    df = pd.read_csv(in_folder / 'log.csv', index_col=0, sep=';')
     try:
-        plot_archive(
-            archive,
-            func,
-            title=plot_title,
-            save_as=plot_dir / f'protoeg-archive-opt-{func.name}-{budget/cost_ratio:.0f}',
-            save_exts=('png',),
-        )
-    except NotImplementedError:
-        pass
+        budget_used = match['budget'] - df['budget'].values
+    except KeyError:  # ignore for now
+        return
+
+    plot_on_axes(axes, budget_used, df)
+    for ext in extensions:
+        fig.savefig(out_folder / f'graphs.{ext}')
+    plt.close()
 
 
-def plot_from_log(df: pd.Dataframe):
+def plot_and_gifify_archives(in_folder: Path, out_folder: Path):
     ...
 
 
-def plot_and_gifify_archives(archives):
+def plot_and_gifify_errorgrids(in_folder: Path, out_folder: Path):
     ...
 
 
-def plot_and_gifify_errorgrids(error_grids):
-    ...
+
+def perform_processing_for(experiment_folder: Path):
+    """Run all relevant processing/plotting actions"""
+    out_folder = plot_path / experiment_folder.name
+    out_folder.mkdir(parents=True, exist_ok=True)
+
+    plot_log(experiment_folder, out_folder)
+    # plot_and_gifify_archives(experiment_folder, out_folder)
+    # plot_and_gifify_errorgrids(experiment_folder, out_folder)
 
 
 
 def main():
-    # for f in data_path.iterdir():
-    #     plot_tracking_for_file(f)
-    new_data_dir = here('files/2021-07-06-manual-additions/')
-
-    for f in new_data_dir.iterdir():
-        # plot_tracking_for_file(f)
-        if f.suffix == '.csv' and 'naive' in f.name:
-            plot_comparison(f, new_data_dir / Path(f.name.replace('naive', 'proto-eg')))
-
-
+    for experiment_folder in data_path.iterdir():
+        perform_processing_for(experiment_folder)
 
 if __name__ == '__main__':
     main()
