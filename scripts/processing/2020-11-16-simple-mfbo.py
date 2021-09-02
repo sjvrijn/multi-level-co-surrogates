186df832381c0e2b5108247f3b406f737649148e
Sander van Rijn <svr003@gmail.com>
2021-09-02 15:44:36 +0200

    initial partial implementation of plot_and_gifify_archives

--- scripts/processing/2020-11-16-simple-mfbo.py
+++ scripts/processing/2020-11-16-simple-mfbo.py
@@ -6,18 +6,34 @@
 files during the simple-mfbo runs
 """
 
-import matplotlib.pyplot as plt
-import pandas as pd
+import pickle
+import sys
 from pathlib import Path
+
+import matplotlib.pyplot as plt
+import mf2
+import pandas as pd
 from parse import compile
 from pyprojroot import here
 
+import processing as proc
+module_path = str(here())
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from multiLevelCoSurrogates import CandidateArchive
 
 data_path = here('files/2020-11-05-simple-mfbo/')
 plot_path = here('plots/2020-11-16-simple-mfbo/', warn=False)
 plot_path.mkdir(exist_ok=True, parents=True)
 
 subfolder_template = compile('{func_name}-{method}-b{budget:d}-i{idx:d}')
+archive_template = compile('archive_{iteration:d}.pkl')
+
+named_functions = {
+    func.name.lower(): func
+    for func in mf2.bi_fidelity_functions
+}
 
 
 def plot_on_axes(axes, budget_used, df, label=''):
@@ -64,16 +80,6 @@ def make_all_plots_for_experiment_directory(experiment_dir: Path):
     #     xlim=(3, init_budget),
     #     ylim=(2, (init_budget // 2)),
     # )
-    # try:
-    #     plot_archive(
-    #         archive,
-    #         func,
-    #         title=plot_title,
-    #         save_as=plot_dir / f'protoeg-archive-opt-{func.name}-{budget/cost_ratio:.0f}',
-    #         save_exts=('png',),
-    #     )
-    # except NotImplementedError:
-    #     pass
     ...
 
 
@@ -102,8 +108,34 @@ def plot_log(in_folder: Path, out_folder: Path, save_exts=('png', 'pdf')) -> Non
     plt.close()
 
 
-def plot_and_gifify_archives(in_folder: Path, out_folder: Path):
-    ...
+def plot_and_gifify_archives(in_folder: Path, out_folder: Path, save_exts=('png', 'pdf')):
+    """Create 2d plots of all archive.pkl files in `in_folder`, including animated GIF
+
+    :param in_folder:  folder containing the log.csv file to read and plot
+    :param out_folder: where to store resulting plots
+    :param save_exts:  which extensions to use when saving the individual plots
+    """
+    match = subfolder_template.parse(in_folder.name)
+    func_name = match['func_name']
+
+    archive_files = [
+        (f, archive_template.parse(f.name)['iteration'])
+        for f in in_folder.iterdir()
+        if archive_template.parse(f.name)
+    ]
+    for archive_file, iteration_idx in archive_files:
+        archive = CandidateArchive(ndim=0, fidelities=['high', 'low', 'high-low'])
+        with open(archive_file, 'rb') as f:
+            archive.data = pickle.load(f)
+        proc.plot_archive(
+            archive,
+            named_functions[func_name.lower()],
+            f'plot of archive at idx {iteration_idx}',
+            save_as=out_folder / archive_file.name,
+            save_exts=save_exts
+        )
+
+    # do_gifification_here()
 
 
 def plot_and_gifify_errorgrids(in_folder: Path, out_folder: Path):
