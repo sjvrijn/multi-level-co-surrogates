85ff59ac47e14643a6d1adaec7652656496c2c65
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2021-12-04 23:45:43 +0100

    add .item() call to processing.get_extent() values

--- scripts/processing/processing.py
+++ scripts/processing/processing.py
@@ -51,10 +51,10 @@ def get_extent(data: xr.DataArray):
     centered in the 'pixels'
     """
     return [
-        np.min(data.n_low) - 0.5,
-        np.max(data.n_low) - 0.5,
-        np.min(data.n_high) - 0.5,
-        np.max(data.n_high) - 0.5,
+        np.min(data.n_low).item() - 0.5,
+        np.max(data.n_low).item() - 0.5,
+        np.min(data.n_high).item() - 0.5,
+        np.max(data.n_high).item() - 0.5,
     ]
 
 
