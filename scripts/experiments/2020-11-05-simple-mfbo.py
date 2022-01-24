ff9f2b067c144366a485e10aa1d00d730f2e4682
Sander van Rijn <svr003@gmail.com>
2022-01-24 12:27:44 +0100

    remove old proto_eg function

--- scripts/experiments/2020-11-05-simple-mfbo.py
+++ scripts/experiments/2020-11-05-simple-mfbo.py
@@ -294,8 +294,7 @@ class Optimizer:
 
 
     def acq_max(self, y_best, random_state, n_warmup=1_000, n_iter=50):
-        """
-        Code adapted from:
+        """Code adapted from:
           https://github.com/fmfn/BayesianOptimization/blob/380b0d52ae0e3650b023c4ef6db43f7343c75dea/bayes_opt/util.py
         Under MIT License
 
@@ -385,96 +384,6 @@ def fit_lin_reg(da: xr.DataArray, calc_SSE: bool=False):
     pred_y = reg.predict(X)
     SSE = np.sum((pred_y - y)**2)
     return reg, SSE
-
-
-def proto_EG_multifid_bo(func, init_budget, cost_ratio, doe_n_high, doe_n_low, run_save_dir, num_reps=50, seed_offset=None):
-    if seed_offset:
-        np.random.seed(RANDOM_SEED_BASE + seed_offset)
-    start = time()
-
-    if doe_n_high + cost_ratio*doe_n_low >= init_budget:
-        raise ValueError('Budget should not be exhausted after DoE')
-
-    mfm_opts = dict(
-        kernel='Matern',
-        scaling='off',
-    )
-    logfile = run_save_dir / 'log.csv'
-    Entry = namedtuple('Entry', 'iteration budget iter_since_high_eval tau fidelity wall_time nhigh nlow reuse_fraction candidate fitness')
-    with open(logfile, 'w') as csvfile:
-        logwriter = writer(csvfile, delimiter=';')
-        logwriter.writerow(Entry._fields)
-    entries = []
-
-    #make mf-DoE
-    high_x, low_x = mlcs.bi_fidelity_doe(func.ndim, doe_n_high, doe_n_low)
-    high_x, low_x = scale_to_function(func, [high_x, low_x])
-    high_y, low_y = func.high(high_x), func.low(low_x)
-
-    #subtract mf-DoE from budget
-    budget = init_budget - (doe_n_high + doe_n_low*cost_ratio)
-
-    #create archive
-    archive = mlcs.CandidateArchive.from_bi_fid_DoE(high_x, low_x, high_y, low_y)
-
-    proto_eg = mlcs.ProtoEG(archive, num_reps=num_reps, interval=2, mfm_opts=mfm_opts)
-    proto_eg.subsample_errorgrid()
-
-    mfm = mlcs.MultiFidelityModel(fidelities=['high', 'low'], archive=archive, **mfm_opts)
-
-    iterations = 0
-    iter_since_high_eval = 0
-    while budget > 0:
-        tau = calc_tau_from_EG(proto_eg.error_grid['mses'], cost_ratio)
-
-        fidelity = 'high' if tau <= iter_since_high_eval and archive.count('high') < archive.count('low') else 'low'
-
-        # predict best place to evaluate:
-        if fidelity == 'high':
-            #best predicted low-fid only datapoint for high-fid (to maintain hierarchical model)
-            candidates = select_high_fid_only_candidates(archive)
-            candidate_predictions = [
-                (cand, mfm.models['high'].predict(cand.reshape(1, -1)))
-                for cand in candidates
-            ]
-
-            x = min(candidate_predictions, key=itemgetter(1))[0].ravel()
-            iter_since_high_eval = 0
-            budget -= 1
-        else:  # elif fidelity == 'low':
-            # simple optimization for low-fid
-            x = minimize(
-                lambda x: mfm.models['high'].predict(x.reshape(1, -1)),
-                x0=np.random.uniform(func.l_bound, func.u_bound).reshape(-1, ),
-                bounds=func.bounds.T,
-            ).x
-
-            while x in archive:  # resample to ensure a new candidate is added to the archive
-                # print(f'Existing candidate {x} ...')
-                random_candidates = scale_to_function(func, np.random.rand(RAND_SAMPLES_PER_DIM * func.ndim, func.ndim))
-                fitnesses = mfm.models['high'].predict(random_candidates)
-                x = random_candidates[np.argmin(fitnesses)]
-                # print(f'... replaced by {x}')
-
-            iter_since_high_eval += 1
-            budget -= cost_ratio
-
-        #evaluate best place
-        y = func[fidelity](x.reshape(1, -1))[0]
-        archive.addcandidate(candidate=x.flatten(), fitness=y, fidelity=fidelity)
-
-        # update model & error grid
-        mfm.retrain()
-        proto_eg.update_errorgrid_with_sample(x, fidelity=fidelity)
-
-        # logging
-        entries.append(Entry(iterations, budget, iter_since_high_eval, tau, fidelity, time()-start, archive.count('high'), archive.count('low'), proto_eg.reuse_fraction, x.flatten(), y))
-        with open(logfile, 'a') as csvfile:
-            logwriter = writer(csvfile, delimiter=';')
-            logwriter.writerow(entries[-1])
-        np.save(run_save_dir / archive_file_template.format(iterations), archive)
-        proto_eg.error_grid.to_netcdf(run_save_dir / errorgrid_file_template.format(iterations))
-        iterations += 1
 
 
 def simple_multifid_bo(func, init_budget, cost_ratio, doe_n_high, doe_n_low, run_save_dir, num_reps=50, seed_offset=None):
