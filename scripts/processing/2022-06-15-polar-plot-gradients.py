b8327c9f44e92002045de94be86b2ed62ddee395
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2022-06-15 16:40:22 +0200

    add polar plot of gradient vs budget

---
+++ scripts/processing/2022-06-15-polar-plot-gradients.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+2022-06-15-polar-plot-gradients.py: script to plot the gradient of a range of
+error grids as polar coordinates (angle, budget_used) for an optimization run.
+"""
+
+import argparse
+from itertools import product
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import mf2
+import numpy as np
+import pandas as pd
+from parse import compile
+from pyprojroot import here
+from tqdm import tqdm
+import xarray as xr
+
+import multiLevelCoSurrogates as mlcs
+
+import processing as proc
+
+data_path = here('files/2020-11-05-simple-mfbo/')
+plot_path = here('plots/2022-06-15-polar-plot-gradients/', warn=False)
+plot_path.mkdir(exist_ok=True, parents=True)
+
+subfolder_template = compile('{func_name}-{method}-b{init_budget:d}-i{idx:d}')
+errorgrid_template = compile('errorgrid_{iteration:d}.nc')
+
+
+def plot_folder_angles_as_polar(folder: Path, exts):
+
+    # todo: rewrite proc.get_gradient_angles to be more generic?
+    # angle_df = proc.get_gradient_angles(folder)
+
+    match = subfolder_template.parse(folder.name)
+    if not match:
+        return
+
+    budgets = [0]
+    init_budget = match['init_budget']
+    df = pd.read_csv(folder / 'log.csv', index_col=0, sep=';')
+    budgets.extend((init_budget - df['budget'].values).tolist())
+
+    angles = []
+    for file in sorted(folder.iterdir()):
+        if not errorgrid_template.parse(file.name):
+            continue
+        with xr.open_dataset(file) as ds:
+            da = ds['mses'].sel(model='high_hier')
+        with da.load() as da:
+            angle_summary = mlcs.utils.error_grids.calc_angle(da)
+        angles.append(angle_summary.theta)
+
+    if not angles:
+        return  # no .nc files were present
+
+    if len(angles) != len(budgets):
+        actual_length = min(len(angles), len(budgets))
+        angles, budgets = angles[:actual_length], budgets[:actual_length]
+
+    fig, ax = plt.subplots(1, 1, subplot_kw={'projection': 'polar'})
+    ax.plot(angles, budgets)
+    for ext in exts:
+        fig.savefig(plot_path / f'{folder.name}{ext}')
+
+
+def main(args):
+    suffixes = args.exts or proc.suffixes
+    for subfolder in sorted(data_path.iterdir()):
+        if not subfolder.is_dir():
+            continue
+        print(subfolder.name)
+        plot_folder_angles_as_polar(subfolder, suffixes)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--exts", action="extend", nargs="+", type=str,
+                        help="File extensions to use when saving images. Default: [.PNG, .PDF].")
+    args = parser.parse_args()
+
+    main(args)
