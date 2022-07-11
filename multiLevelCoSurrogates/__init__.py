eb3ed1d538e8733edc65ec5f9b29feba24872dd8
S.J. van Rijn <s.j.van.rijn@liacs.leidenuniv.nl>
2022-07-11 11:55:39 +0200

    Add acquisition and optimizer to mlcs.__init__

--- multiLevelCoSurrogates/__init__.py
+++ multiLevelCoSurrogates/__init__.py
@@ -9,12 +9,14 @@ __author__ = 'Sander van Rijn'
 __email__ = 's.j.van.rijn@liacs.leidenuniv.nl'
 
 
+from .acquisition import *
 from .candidate_archive import *
+from .instance import *
+from .optimizer import *
+from .protoEG import *
 from .surrogates import *
 from .utils import *
-from .instance import *
 
 
 from .bifidbo import *
 from .multiFidBO import *
-from .protoEG import *
